d2891dacac15a39ab9da8950c2102cdae597dad3
Matt Chun-Lum <mchunlum@gmail.com>
2012-04-22 21:44:05 -0700

    started base template file

---
+++ base.rb
@@ -0,0 +1,17 @@
+# base.rb rails applicaiton template
+
+# cleanup
+run "rm public/index.html"
+run "echo TODO > README"
+
+# setup gems
+gem 'devise'
+gem 'activeadmin'
+
+
+# git
+run "echo database.yml > config/.gitignore"
+git :init
+git :add => "."
+git :commit => "-a -m 'initial commit'"
+
